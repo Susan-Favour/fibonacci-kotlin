f7dc60991bd8633e17090cb78830df4e1bd1db50
SusanFavour <susanfavourodongo@gmail.com>
2024-06-20 15:18:51 +0300

    add files

---
+++ app/src/main/java/com/example/fibonacciassignment/FibonacciRecycleViewAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.fibonacciassignment
+
+import android.view.View
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+
+class FibonacciRecyclerViewAdapter (var numbers:List<Int>): RecyclerView.Adapter<NumbersViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NumbersViewHolder {
+        val numberView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.fibonacci_number_views,parent, false)
+        return NumbersViewHolder(numberView)
+    }
+
+    override fun onBindViewHolder(holder: NumbersViewHolder, position: Int) {
+        holder.tvNumbers.text = numbers[position].toString()
+
+    }
+
+    override fun getItemCount(): Int {
+        return numbers.size
+    }
+
+}
+
+class NumbersViewHolder(numberView:View):RecyclerView.ViewHolder(numberView){
+    val tvNumbers = numberView.findViewById<TextView>(R.id.tvNumbers)
+}
